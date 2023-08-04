d4028af3b4f3e83e748ee00662f92143977b8b4f
Buntueng Yana <mr.buntueng@gmail.com>
2023-08-04 16:38:38 +0700

    update code in collectionviews_example

---
+++ controls/collectionview_example/collectionview_example/views/collectionview_view.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collectionview_example.views
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string PlayerImage { get; set; }
+        public string Country { get; set; }
+
+        ObservableCollection<Player> players = new ObservableCollection<Player>();
+        public ObservableCollection<Player> Players { get { return players; } }
+
+
+
+    }
+
+
+
+}
