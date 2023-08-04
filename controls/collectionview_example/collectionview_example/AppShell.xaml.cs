68b235be42c5680ecfc0074fd316d0d8ca68b9d3
Buntueng Yana <mr.buntueng@gmail.com>
2023-08-04 16:18:01 +0700

    add collectionview_example project

---
+++ controls/collectionview_example/collectionview_example/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace collectionview_example
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ controls/collectionview_example/collectionview_example/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace collectionview_example
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
